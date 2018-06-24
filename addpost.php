796c50e19c454ba0966b316e4e939d672be11326
root <root@lsa-template.local.asl24.pl>
2018-06-24 05:23:51 +0200

    List all

--- addpost.php
+++ addpost.php
@@ -27,32 +27,32 @@
 </head>
 <body>
 <?PHP
-if($_SERVER["REQUEST_METHOD"] == "POST")
-{
 
+if($_SERVER["REQUEST_METHOD"] == "POST")
+
+{
 
 $connection = new AMQPStreamConnection('192.168.100.10', 5672, 'admin', 'admin');
 
 $channel = $connection->channel();
 
-$post = $_POST['posts'];
 
 
 $channel->queue_declare('task_queue', false, true, false, false);
 
 
 
-$posts = implode('', array_slice($argv, 1));
+$data = implode(' ', array_slice($argv, 1));
 
-if (empty($posts)) {
+if (empty($data)) {
 
-    $posts = $post;
+    $data = "Hello World!";
 
 }
 
 $msg = new AMQPMessage(
 
-    $posts,
+    $data,
 
     array('delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT)
 
@@ -64,52 +64,56 @@ $channel->basic_publish($msg, '', 'task_queue');
 
 
 
-echo ' [x] Sent ', $posts, "\n";
+echo ' [x] Sent ', $data, "\n";
 
 
 
 $channel->close();
 
 $connection->close();
+	$post = $_POST['posts'];
 	$sql = "INSERT INTO posts (id,post) VALUE ('".$user['id']."','$post')";
-	$dbm = mysqli_connect(DB_SERVER_M,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-	$res = mysqli_query($dbm, $sql);
-}
 
-mysqli_close($dbm);
+	$dbm = mysqli_connect(DB_SERVER_M,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+	$res = mysqli_query($dbm, $sql);
+	
+	mysqli_close($dbm);
+}
 ?>
 <form method="post" action="addpost.php">
 	<br><input type="text" name="posts" maxlength="255"></br>
 	<br><input type="submit" name="send"value="SEND POST"></br>
 </form>
 POSTY:
+<br>
 
 <?PHP
-  $redisClient = new Redis();
-
-  $redisClient->connect( REDIS_SERVER, REDIS_PORT );
-  $redisClient->auth(REDIS_PASSWORD);
   $posty = array();
   $sql1 = "select * from posts ORDER BY date DESC LIMIT 10";
   $dbs = mysqli_connect(DB_SERVER_S,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-  if (($posty = $redisClient->get("last_10_posts")) == NULL)
+
+  if ($ifRedisexists = redis_get_json("last_10_posts") === NULL)
   {
+	//execute query if we don't have 10 posts in Redis
 	$res1 = mysqli_query($dbs,$sql1);
+	$arraywithPosts = array();
 	while($row = mysqli_fetch_array($res1)) {
-		redis_set_json("last_10_posts",$row,15);
-		$post =  redis_get_json(last_10_posts);
-		echo $post['post']."<br>";
+		$arraywithPosts[] = $row;
 	}
+	redis_set_json("last_10_posts",$arraywithPosts,30);
  }
   else
-   {	
-	$post =  redis_get_json(last_10_posts);
-	
-	print_r($post['post']);
+   {
+	$posty =  redis_get_json("last_10_posts");
    }
 
-  mysqli_close($dms);
-$redisClient->close();
+   foreach ($posty as &$mypost)
+	{
+		echo $mypost['post']."<br>";
+	}
+
+mysqli_close($dbs);
 ?>
 </body>
 </html>
